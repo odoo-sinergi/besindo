67c36515d240a5d0124b777ef3e7b3f6c7f29c08
damongrumi <damongrumi@gmail.com>
2023-08-21 14:33:37 +0700

    add new bool field to stock.picking and its relation to stock.picking.type . if the value of this field is true, actual_date in stock.picking will be showed

--- sdt_udf_besindo/models/stock_picking.py
+++ sdt_udf_besindo/models/stock_picking.py
@@ -8,6 +8,7 @@ class StockPicking(models.Model):
     actual_date = fields.Datetime(string='Actual Delivery Date')
     workcenter_name = fields.Char(string='Workcenter Name', compute='_get_workcenter')
     is_qc_production = fields.Boolean(string='Is QC Production',related='picking_type_id.is_qc_production',readonly=True, store=True)
+    different_delivery_date = fields.Boolean(string='Different Delivery Date',related='picking_type_id.different_delivery_date',readonly=True, store=True)
 
     @api.depends('location_id','location_dest_id','date_done','move_ids_without_package','scheduled_date')
     def _get_workcenter(self):
