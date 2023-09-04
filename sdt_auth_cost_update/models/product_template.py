ca2515410b16effc3e0bd2adfa8e80d0fd4ac7d5
damongrumi <damongrumi@gmail.com>
2023-09-04 16:23:51 +0700

    update

--- sdt_auth_cost_update/models/product_template.py
+++ sdt_auth_cost_update/models/product_template.py
@@ -15,24 +15,24 @@ class ProductTemplate(models.Model):
     is_show_inventory_cost = fields.Boolean(string='Is Show Inventory Cost', readonly=True, compute='_get_auth_user_cost')
     is_show_inventory_price = fields.Boolean(string='Is Show Inventory Cost', readonly=True, compute='_get_auth_user_price')
 
-    def _get_auth_user(self, called_from):
-        for line in self:
-            user_obj = self.env['res.users'].search([('id', '=', line._uid)])
-            if called_from == 'cost':
-                if user_obj.show_inventory_cost == True:
-                    line.is_show_inventory_cost=True
-                else:
-                    line.is_show_inventory_cost=False
-            else:
-                if user_obj.show_inventory_price == True:
-                    line.is_show_inventory_price=True
-                else:
-                    line.is_show_inventory_price=False
+    # def _get_auth_user(self, called_from):
+    #     for line in self:
+    #         user_obj = self.env['res.users'].search([('id', '=', line._uid)])
+    #         if called_from == 'cost':
+    #             if user_obj.show_inventory_cost == True:
+    #                 line.is_show_inventory_cost=True
+    #             else:
+    #                 line.is_show_inventory_cost=False
+    #         else:
+    #             if user_obj.show_inventory_price == True:
+    #                 line.is_show_inventory_price=True
+    #             else:
+    #                 line.is_show_inventory_price=False
 
-    @api.depends()
-    def _get_auth_user_cost(self):
-        self._get_auth_user('cost')
+    # @api.depends()
+    # def _get_auth_user_cost(self):
+    #     self._get_auth_user('cost')
 
-    @api.depends()
-    def _get_auth_user_price(self):
-        self._get_auth_user('price')
+    # @api.depends()
+    # def _get_auth_user_price(self):
+    #     self._get_auth_user('price')
