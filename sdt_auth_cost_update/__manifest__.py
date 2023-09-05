d275b20d222456812c0c18edcceef97457af3ed5
damongrumi <damongrumi@gmail.com>
2023-09-05 09:20:18 +0700

    up

--- sdt_auth_cost_update/__manifest__.py
+++ sdt_auth_cost_update/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','product', 'stock','account','sale'],
+    'depends': ['base','product', 'stock','account','sale', 'sale_margin'],
 
     # always loaded
     'data': [
