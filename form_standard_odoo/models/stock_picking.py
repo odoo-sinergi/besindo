d707b5ae22c6316e27219bd27cf1c3dd93c34795
damongrumi <damongrumi@gmail.com>
2023-09-07 14:34:16 +0700

    update contact auto fill from mo to picking

--- form_standard_odoo/models/stock_picking.py
+++ form_standard_odoo/models/stock_picking.py
@@ -7,7 +7,11 @@ class StockPicking(models.Model):
 
     mrp_id = fields.Many2one('mrp.production', string = 'MO Origin')
 
+    @api.depends('origin')
+    def partner_origin(self):
+        self.partner_id = self.env['mrp.production'].search([('name', '=', self.origin)]).contact
+
     @api.onchange('origin')
     def onchange_mrp_id(self):
         self.mrp_id = self.env['mrp.production'].search([('name', '=', self.origin)])
-        self.partner_id = self.mrp_id.contact
+        # self.partner_id = self.mrp_id.contact
