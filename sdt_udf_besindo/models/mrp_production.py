071dd203f65930571c3d0dedda3f3841e0a51b0a
damongrumi <damongrumi@gmail.com>
2023-09-07 16:21:24 +0700

    update mrp

--- sdt_udf_besindo/models/mrp_production.py
+++ sdt_udf_besindo/models/mrp_production.py
@@ -6,6 +6,8 @@ from odoo.tools import float_compare, float_round, format_datetime
 class MRPProduction(models.Model):
     _inherit = "mrp.production"
 
+    contact = fields.Many2one("res.partner", string="Contact", domain=[('category_id.name','=','Customer')], required=True)
+
     @api.onchange('workorder_ids','workorder_ids.workcenter_id')
     def _onchange_workcenter_id(self):
         for workorder_id in self.workorder_ids :
@@ -18,7 +20,37 @@ class MRPProduction(models.Model):
                     pass
             else :
                 workorder_id.name = ""
-    
+
+    def action_confirm(self):
+        self._check_company()
+        for production in self:
+            if production.bom_id:
+                production.consumption = production.bom_id.consumption
+            # In case of Serial number tracking, force the UoM to the UoM of product
+            if production.product_tracking == 'serial' and production.product_uom_id != production.product_id.uom_id:
+                production.write({
+                    'product_qty': production.product_uom_id._compute_quantity(production.product_qty, production.product_id.uom_id),
+                    'product_uom_id': production.product_id.uom_id
+                })
+                for move_finish in production.move_finished_ids.filtered(lambda m: m.product_id == production.product_id):
+                    move_finish.write({
+                        'product_uom_qty': move_finish.product_uom._compute_quantity(move_finish.product_uom_qty, move_finish.product_id.uom_id),
+                        'product_uom': move_finish.product_id.uom_id
+                    })
+            production.move_raw_ids._adjust_procure_method()
+            (production.move_raw_ids | production.move_finished_ids)._action_confirm(merge=False)
+            production.workorder_ids._action_confirm()
+        # run scheduler for moves forecasted to not have enough in stock
+        self.move_raw_ids._trigger_scheduler()
+        self.picking_ids.filtered(
+            lambda p: p.state not in ['cancel', 'done']).action_confirm()
+        # Force confirm state only for draft production not for more advanced state like
+        # 'progress' (in case of backorders with some qty_producing)
+        self.filtered(lambda mo: mo.state == 'draft').state = 'confirmed'
+        
+        for pick in self.picking_ids:
+            pick.partner_id = self.contact
+        return True
 
 class MRPWorkOrder(models.Model):
     _inherit = "mrp.workorder"
@@ -26,6 +58,7 @@ class MRPWorkOrder(models.Model):
 
     operator_factory_ids = fields.Many2many('sdt.operator.factory', string='Operator Factory')
     qty_remaining2 = fields.Float('Quantity to Produced', compute='_compute_qty_remaining2', digits='Product Unit of Measure', store=True)
+    shift = fields.Selection([('shift_1', 'Shift 1'), ('shift_2', 'Shift 2')], string="Shift", required=True, default='shift_1')
 
 
     @api.depends('qty_production', 'qty_reported_from_previous_wo', 'qty_produced', 'production_id.product_uom_id')
