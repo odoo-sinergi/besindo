51d85102bc40630576a822f95cc01dbd26356208
damongrumi <damongrumi@gmail.com>
2023-09-07 16:17:48 +0700

    update contact auto from mo

--- sdt_udf_besindo/models/mrp_production.py
+++ sdt_udf_besindo/models/mrp_production.py
@@ -20,14 +20,37 @@ class MRPProduction(models.Model):
                     pass
             else :
                 workorder_id.name = ""
-    
-    @api.depends('contact')
-    def _onchange_contact(self):
-        if self.picking_ids:
-            for picking in self.picking_ids:
-                picking.partner_id = self.contact
-        # test = self
-    
+
+    def action_confirm(self):
+        self._check_company()
+        for production in self:
+            if production.bom_id:
+                production.consumption = production.bom_id.consumption
+            # In case of Serial number tracking, force the UoM to the UoM of product
+            if production.product_tracking == 'serial' and production.product_uom_id != production.product_id.uom_id:
+                production.write({
+                    'product_qty': production.product_uom_id._compute_quantity(production.product_qty, production.product_id.uom_id),
+                    'product_uom_id': production.product_id.uom_id
+                })
+                for move_finish in production.move_finished_ids.filtered(lambda m: m.product_id == production.product_id):
+                    move_finish.write({
+                        'product_uom_qty': move_finish.product_uom._compute_quantity(move_finish.product_uom_qty, move_finish.product_id.uom_id),
+                        'product_uom': move_finish.product_id.uom_id
+                    })
+            production.move_raw_ids._adjust_procure_method()
+            (production.move_raw_ids | production.move_finished_ids)._action_confirm(merge=False)
+            production.workorder_ids._action_confirm()
+        # run scheduler for moves forecasted to not have enough in stock
+        self.move_raw_ids._trigger_scheduler()
+        self.picking_ids.filtered(
+            lambda p: p.state not in ['cancel', 'done']).action_confirm()
+        # Force confirm state only for draft production not for more advanced state like
+        # 'progress' (in case of backorders with some qty_producing)
+        self.filtered(lambda mo: mo.state == 'draft').state = 'confirmed'
+        
+        for pick in self.picking_ids:
+            pick.partner_id = self.contact
+        return True
 
 class MRPWorkOrder(models.Model):
     _inherit = "mrp.workorder"
