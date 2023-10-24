a283e0e5b52d8639a8a975f4e0dd0562539bc0bc
aditya_demas <adityademas@gmail.com>
2023-10-24 09:03:48 +0700

    up

--- sdt_udf_besindo/models/purchase_order.py
+++ sdt_udf_besindo/models/purchase_order.py
@@ -95,36 +95,56 @@ class PurchaseOrder(models.Model):
                     if approval_po.currency_id.display_name == 'IDR' :
                         if rec.amount_total >= approval_po.min_approve_lvl_2_po:
                             for user_id in approval_po.approver_ids:
-                                approvals_id = self.env['approval.request'].sudo().create({
-                                'name':'Approval/PO/-'+self.name,
-                                'date' : fields.Datetime.now(),
-                                'reference':rec.name,
-                                'category_id' : approval_po.id,
-                                'purchase_order_id' : self.id,
-                                'lvl_approver' : user_id.lvl_approver,
-                                'request_owner_id' : self.env.uid,
-                                'request_status' : 'pending',
-                                'amount' : self.amount_total,
-                                })
-                                for line_id in self.order_line:
-                                    vals ={
-                                        'approval_request_id': approvals_id.id,
-                                        'product_id': line_id.product_id.id,
-                                        'description': line_id.name,
-                                        'quantity': line_id.product_uom_qty,
-                                        'product_uom_id': line_id.product_uom.id,
-                                    }
-                                    self.env['approval.product.line'].create(vals)
-                                for approver_id in approvals_id.approver_ids :
-                                    approver_id.unlink()
-                                if user_id :
+                                sql_query="""
+                                    select count(1) from approval_request where purchase_order_id= %s and lvl_approver = %s
+                                """
+                                self.env.cr.execute(sql_query, (rec.id, user_id.lvl_approver,))
+                                check_po = self.env.cr.dictfetchall()
+                                check_po = check_po[0]['count']
+                                if check_po == 0 :
+                                    approvals_id = self.env['approval.request'].sudo().create({
+                                    'name':'Approval/PO/-'+self.name,
+                                    'date' : fields.Datetime.now(),
+                                    'reference':rec.name,
+                                    'category_id' : approval_po.id,
+                                    'purchase_order_id' : self.id,
+                                    'lvl_approver' : user_id.lvl_approver,
+                                    'request_owner_id' : self.env.uid,
+                                    'request_status' : 'pending',
+                                    'amount' : self.amount_total,
+                                    })
+                                    for line_id in self.order_line:
+                                        vals ={
+                                            'approval_request_id': approvals_id.id,
+                                            'product_id': line_id.product_id.id,
+                                            'description': line_id.name,
+                                            'quantity': line_id.product_uom_qty,
+                                            'product_uom_id': line_id.product_uom.id,
+                                        }
+                                        self.env['approval.product.line'].create(vals)
+                                    for approver_id in approvals_id.approver_ids :
+                                        approver_id.unlink()
+                                    if user_id :
+                                        approvals_id.approver_ids += self.env['approval.approver'].create({
+                                            'user_id': user_id.user_id.id,
+                                            'request_id': approvals_id.id,
+                                            'status': 'new',
+                                            'company_id': rec.company_id.id,
+                                        })
+                                    approvals_id.action_confirm()
+                                else :
+                                    sql_query="""
+                                        select id from approval_request where purchase_order_id= %s and lvl_approver = %s
+                                    """
+                                    self.env.cr.execute(sql_query, (rec.id, user_id.lvl_approver,))
+                                    id_aq = self.env.cr.dictfetchall()
+                                    id_aq = id_aq[0]['id']
                                     approvals_id.approver_ids += self.env['approval.approver'].create({
                                         'user_id': user_id.user_id.id,
-                                        'request_id': approvals_id.id,
-                                        'status': 'new',
+                                        'request_id': id_aq,
+                                        'status': 'pending',
                                         'company_id': rec.company_id.id,
                                     })
-                                approvals_id.action_confirm()
                         else :
                             for user_id in approval_po.approver_ids:
                                 if user_id.lvl_approver == 1 :
@@ -192,9 +212,8 @@ class PurchaseOrder(models.Model):
                                 })
                             approvals_id.action_confirm()
                 rec.req_approval = True
-
-
             rec.req_approval = True
+            
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
