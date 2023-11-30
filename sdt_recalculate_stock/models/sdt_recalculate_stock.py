c73abcf4d8e7f7cac09790ff83036e9da9594cde
damongrumi <damongrumi@gmail.com>
2023-11-30 13:28:46 +0700

    update calculation

--- sdt_recalculate_stock/models/sdt_recalculate_stock.py
+++ sdt_recalculate_stock/models/sdt_recalculate_stock.py
@@ -10,13 +10,29 @@ class RecalculateStock(models.TransientModel):
         # today= datetime.today().strftime('%Y-%m-%d')
         loc_obj = self.env['stock.location'].search([('is_recalculate', '=', True)])
         for loc in loc_obj:
-            sql_query = """UPDATE stock_quant SET reserved_quantity=0 WHERE location_id=%s;
-                UPDATE stock_quant AS b SET reserved_quantity=a.product_qty
-                FROM (SELECT product_id, location_id, sum(qty_done) AS product_qty
-                    FROM stock_move_line
-                    WHERE state in ('confirmed','assigned','partially_available')
-                    GROUP BY product_id,location_id,result_package_id) a
-                WHERE b.product_id=a.product_id AND b.location_id=a.location_id
-                AND a.product_qty<>b.reserved_quantity and a.location_id=%s;
-                """
-            self.env.cr.execute(sql_query, (loc.id, loc.id))
+            sql_query = """UPDATE
+                                stock_quant as q
+                            SET
+                                reserved_quantity = x.move_line
+                            FROM
+                                (SELECT
+                                    sq.id, sml.product_id, sml.reference, sq.location_id,
+                                    sml.qty_done as move_line, sq.reserved_quantity as quant
+                                FROM
+                                    stock_move_line sml
+                                JOIN
+                                    stock_quant sq
+                                ON              
+                                    sml.product_id = sq.product_id
+                                    AND sml.location_id = sq.location_id
+                                    AND (sml.qty_done = sq.reserved_quantity + 0.01
+                                    OR sml.qty_done = sq.reserved_quantity + 0.02)
+                                WHERE
+                                    sml.location_id = %s
+                                    AND sml.state in ('assigned')
+                                    AND (sq.reserved_quantity + 0.01 <= sq.quantity OR sq.reserved_quantity + 0.02 <= sq.quantity)) x
+                            WHERE 
+                                q.id = x.id;
+                            """%(loc.id)
+            test = self.env.cr.execute(sql_query)
+            return test
