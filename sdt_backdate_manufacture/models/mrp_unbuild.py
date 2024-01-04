23e5e5d933040bb3db4275d2c9df5afde27d8d1f
damongrumi <damongrumi@gmail.com>
2024-01-04 09:57:36 +0700

    update mrp unbuild backdate

--- sdt_backdate_manufacture/models/mrp_unbuild.py
+++ sdt_backdate_manufacture/models/mrp_unbuild.py
@@ -27,36 +27,60 @@ class SdtMrpUnbuild(models.Model):
         
         res = super(SdtMrpUnbuild, self).action_validate()
 
-        if self.produce_line_ids:
+        if self.produce_line_ids or self.consume_line_ids:
             if isinstance(self.unbuild_date, str) == True:
                 self.unbuild_date = datetime.strptime(self.unbuild_date, "%Y-%m-%d %H:%M:%S")
             user_tz = self.env.user.tz or pytz.utc
             local = pytz.timezone(user_tz)
             local_date = pytz.utc.localize(self.unbuild_date).astimezone(local)
             user_date = local_date.replace(tzinfo=None)
+            if self.produce_line_ids:
+                for move in self.produce_line_ids:
+                    move.write({'date': self.unbuild_date})
+                    for line in move.move_line_ids:
+                        line.write({'date': self.unbuild_date})
 
-            for move in self.produce_line_ids:
-                move.write({'date': self.unbuild_date})
-                for line in move.move_line_ids:
-                    line.write({'date': self.unbuild_date})
+                    for rec in move.stock_valuation_layer_ids:
+                        sql_query="""Update stock_valuation_layer set create_date=%s where stock_move_id=%s
+                                """
+                        self.env.cr.execute(sql_query,(self.unbuild_date,move.id))
+                    
+                    for account in move.account_move_ids:
+                        if account:
+                            name = account.name.split('/')
+                            if name[0] == 'STJ' and name[1] != str(user_date.year):
+                                query = """update account_move set name = %s , date = %s where id = %s"""
+                                seq = self.env['ir.sequence'].search([('name', '=', 'STJ Sequence')])
+                                new_sequence = seq.next_by_id(user_date)
+                                self.env.cr.execute(query, (new_sequence, str(user_date), account.id))
+                            else:
+                                account.write({'date': user_date})
+                        for account_line in account.invoice_line_ids:
+                            account_line.write({'date': user_date})
+                            
+            if self.consume_line_ids:
+                for move in self.consume_line_ids:
+                    move.write({'date': self.unbuild_date})
+                    for line in move.move_line_ids:
+                        line.write({'date': self.unbuild_date})
 
-                for rec in move.stock_valuation_layer_ids:
-                    sql_query="""Update stock_valuation_layer set create_date=%s where stock_move_id=%s
-                            """
-                    self.env.cr.execute(sql_query,(self.unbuild_date,move.id))
-                
-                for account in move.account_move_ids:
-                    if account:
-                        name = account.name.split('/')
-                        if name[0] == 'STJ' and name[1] != str(user_date.year):
-                            query = """update account_move set name = %s , date = %s where id = %s"""
-                            seq = self.env['ir.sequence'].search([('name', '=', 'STJ Sequence')])
-                            new_sequence = seq.next_by_id(user_date)
-                            self.env.cr.execute(query, (new_sequence, str(user_date), account.id))
-                        else:
-                            account.write({'date': user_date})
-                    for account_line in account.invoice_line_ids:
-                        account_line.write({'date': user_date})
+                    for rec in move.stock_valuation_layer_ids:
+                        sql_query="""Update stock_valuation_layer set create_date=%s where stock_move_id=%s
+                                """
+                        self.env.cr.execute(sql_query,(self.unbuild_date,move.id))
+                    
+                    for account in move.account_move_ids:
+                        if account:
+                            name = account.name.split('/')
+                            if name[0] == 'STJ' and name[1] != str(user_date.year):
+                                query = """update account_move set name = %s , date = %s where id = %s"""
+                                seq = self.env['ir.sequence'].search([('name', '=', 'STJ Sequence')])
+                                new_sequence = seq.next_by_id(user_date)
+                                self.env.cr.execute(query, (new_sequence, str(user_date), account.id))
+                            else:
+                                account.write({'date': user_date})
+                        for account_line in account.invoice_line_ids:
+                            account_line.write({'date': user_date})
         else:
             return res
         
